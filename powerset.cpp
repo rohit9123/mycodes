a7bb9f537dc2418f8ae25796c67f5500d5444606
rohit__143 <rohitkumpan01@gmail.com>
2020-12-29 16:18:46 +0530

    rec

--- powerset.cpp
+++ powerset.cpp
@@ -1,11 +1,37 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+
+
+void findans(vector<int>&sets,vector<vector<int> >&rec,int index){
+    if(index<0){
+        vector<int> k;
+        rec.push_back(k);
+        return;
+    }
+    findans(sets,rec,index-1);
+    int siz=rec.size();
+    int s=sets[index];
+    for(int i=0;i<siz;i++){
+        vector<int> k;
+        for(int j:rec[i]){
+            k.push_back(j);
+        }
+        k.push_back(s);
+        rec.push_back(k);
+    }
+
+}
+
+
+
 int main(){
 vector<int> sets;
 for(int i=1;i<=4;i++){
     sets.push_back(i);
 }
+vector<vector<int> >rec;
+findans(sets,rec,sets.size()-1);
 vector<vector<int> >powerset;
 vector<int> k;
 powerset.push_back(k);
@@ -28,8 +54,8 @@ for(int i=0;i<sets.size();i++){
     
 
 }
- for(int j=0;j<powerset.size();j++){
-        for(int z:powerset[j]){
+ for(int j=0;j<rec.size();j++){
+        for(int z:rec[j]){
             cout<<z<<" ";
         }
         cout<<endl;
